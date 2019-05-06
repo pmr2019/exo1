269033fda43662f442d2d8ce4c87cef52c4af5ad
tb59 <thomas.bourdeaudhuy@gmail.com>
2019-05-06 17:29:51 +0200

    clics sur btnOK

--- app/src/main/java/com/example/pmr_exo1/MainActivity.java
+++ app/src/main/java/com/example/pmr_exo1/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.pmr_exo1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public final String CAT="PMR";
 
@@ -20,11 +23,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         alerter("onCreate");
+
+        Button btnOK = findViewById(R.id.btnOK);
+        EditText edtPseudo = findViewById(R.id.edtPseudo);
+
+
+        // Seconde stratégie
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alerter("clic par instanciation d'un nouvel onclicklistener spécifique pour le clic sur le bouton");
+            }
+        });
+
+
+
+        // Troisième stratégie
+        // L'activité implémente l'interface 'onClickListener'
+        // Une 'interface' est un "contrat"
+        // qui définit des fonctions à implémenter
+        // Ici, l'interface "onClickLister" demande que la classe
+        // qui l'implémente fournisse une méthode onClick
+
+        btnOK.setOnClickListener(this);
+        edtPseudo.setOnClickListener(this);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         alerter("onStart");
+    }
+
+    // Première stratégie
+    public void foo(View view) {
+        alerter("clic par attribut onClick sur la vue dans le layout");
+    }
+
+    @Override
+    public void onClick(View v) {
+        alerter("clic par l'activité qui implémenter l'interface onClickListener");
+        // Intérêt : permet de capturer des clics sur plusieurs éléments...
+        // Démonstration :
+        switch (v.getId()) {
+            case R.id.btnOK :
+                    alerter("Clic sur Bouton");
+                break;
+
+            case R.id.edtPseudo :
+                alerter("Clic sur champ texte");
+                break;
+
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
