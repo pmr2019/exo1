78d4b93523bde1f0d8efe34c999fa8249fd209e8
tb59 <thomas.bourdeaudhuy@gmail.com>
2019-05-06 15:58:09 +0200

    Traces d'exécution dans onCreate & onStart

--- app/src/main/java/com/example/pmr_exo1/MainActivity.java
+++ app/src/main/java/com/example/pmr_exo1/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.pmr_exo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final String CAT="PMR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(CAT,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(CAT,"onStart");
     }
 }
