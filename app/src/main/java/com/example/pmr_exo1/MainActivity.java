a49355b240b4649140ba12002482b6a0708b38b6
tb59 <thomas.bourdeaudhuy@gmail.com>
2019-05-06 17:05:26 +0200

    toast et fonction alerter

--- app/src/main/java/com/example/pmr_exo1/MainActivity.java
+++ app/src/main/java/com/example/pmr_exo1/MainActivity.java
@@ -3,21 +3,28 @@ package com.example.pmr_exo1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public final String CAT="PMR";
 
+    private void alerter(String s) {
+        Log.i(CAT,s);
+        Toast myToast = Toast.makeText(this,s,Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(CAT,"onCreate");
+        alerter("onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(CAT,"onStart");
+        alerter("onStart");
     }
 }
